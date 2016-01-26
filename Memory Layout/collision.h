f70afa33ef85231be185db879d9e435cabd15d6d
esmelusina <esmelusina@gmail.com>
2016-01-26 15:00:09 -0800

    curves and cleanup

--- Memory Layout/collision.h
+++ Memory Layout/collision.h
@@ -3,7 +3,7 @@
 
 struct CollisionData // Wrapper for Minimum Translation Vector
 {
-    bool collides;
+    bool collides;          // true-false
     float PenetrationDepth; // -Distance
     vec2  CollisionNormal;  // Direction
 };
@@ -14,10 +14,13 @@ float ray_plane_dist    (const ray  &a, const plane &b);
 
 // SAT algorithm!
 CollisionData sat_hull          (const ConvexHull &A, const ConvexHull &B);
-CollisionData sat_hull_circle   (const ConvexHull &A, const circle     &b);
-CollisionData sat_hull_ray      (const ConvexHull &A, const ray        &b);
-CollisionData sat_hull_plane    (const ConvexHull &A, const plane      &b);
-CollisionData sat_hull_AABB     (const ConvexHull &A, const aabb       &b);
+
+// optimized edge-cases
+CollisionData sat_hull_circle   (const ConvexHull &A, const circle     &b); // use radius in all axes of convex hull
+CollisionData sat_hull_ray      (const ConvexHull &A, const ray        &b); // combination of circle and plane algorithms*
+CollisionData sat_hull_plane    (const ConvexHull &A, const plane      &b); // only axis is the plane
+CollisionData sat_hull_AABB     (const ConvexHull &A, const aabb       &b); // aabb only contributes 2 axes
+
 
 // determine whether or not the shapes are overlapping... fast!
 bool iTest_aabb         (const aabb   &a, const aabb   &b);
@@ -28,6 +31,7 @@ bool iTest_circle       (const circle &a, const circle &b);
 bool iTest_circle_plane (const circle &a, const plane  &b);
 bool iTest_circle_ray   (const circle &a, const ray    &b);
 bool iTest_ray_plane    (const ray    &a, const plane  &b);
+
 
 // Calculate the Minimum Translation Vector
 // same algorithms as the fast ones, but we're fetching more data
